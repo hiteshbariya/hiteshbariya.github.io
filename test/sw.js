ac1275b68be912c7111586ec13db4a3b4f36892e
Htiesh Bariya <newstohitesh@gmail.com>
2019-06-20 12:46:44 +0530

    test

---
+++ test/sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', function(e) {
+  console.log('1');
+ e.waitUntil(
+   caches.open('video-store').then(function(cache) {
+    console.log('2');
+     return cache.addAll([
+       '',
+       'index.html',
+       'index.js',
+       'css/style.css'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      console.log('3');
+      return response || fetch(e.request);
+      
+    })
+  );
+});
